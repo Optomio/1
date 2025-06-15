c80299ad4715ae79e27cda1844a67440162dd969
Optomio <alessandro.menini99@gmail.com>
2025-06-15 22:52:57 +0200

    Mark.m

---
+++ Mark.m
@@ -0,0 +1,15 @@
+n = 3;
+rendimenti_attesi = [0.3 0.5 0.4];
+covarianza = [0.10 0.02 -0.04;
+              0.02 0.12 0;
+             -0.04 0 0.11];
+A = -rendimenti_attesi;   % vincolo: rendimento ≥ 0.1
+b = -0.1;
+Aeq = ones(1,n);
+beq = 1;
+LB = zeros(n,1);
+X0 = ones(n,1)/n;  % oppure [0.2; 0.3; 0.5];
+options = optimoptions('fmincon', 'Algorithm', 'interior-point', 'Display', 'iter-detailed','PlotFcn', 'optimplotx', 'EnableFeasibilityMode', true);  % ATTIVATO
+[X, fval, exitflag] = fmincon(@(x) x'*covarianza*x, X0, A, b, Aeq, beq, LB, [], [], options);
+disp('Soluzione ottima:');
+disp(X)

a0a68d300c0df3fae2065ca5d1269290423bfb71
Optomio <alessandro.menini99@gmail.com>
2025-06-15 18:45:46 +0200

    ottimizzazione.m

--- Identificazione_ARMA.m
+++ Identificazione_ARMA.m
@@ -1,85 +1,26 @@
 clear
 clc
 close all
-
-%carica
-figure(1)
-plot(y_ARMA) %grafico serie storica
-figure(2)
-autocorr(y_ARMA) %grafico l'autocorrelazione totale
-figure(3)
-parcorr(y_ARMA) %grafico l'autocorrelazione parziale
-%Verificato l'andamento degli autocorrelogrammi posso definire un modello
-%ARMA
-
-%Inizio ad identificare la parte AR di un ARMA con le equazioni di Youle
-%Walker
-
-N=3000; %utilizzo solo questi dati per l'identificazione
-y=y_ARMA(101:N+100);
-[acf,lang,bounds]=autocorr(y);
-
-%Non avendo l'ordine vado per tentativi cambiando n_D
-n_D=3;
-
-%Scrivo la matrice quadrata R che inizializzo come matrice di zeri per poi
-%andarla a riempire
-R=zeros(n_D);
-acf=acf(2:end);
-%Costruisco le colonne di R
-
-for kk=1:n_D
-    R(:,kk)=acf(n_D+1-kk:2*n_D-kk);
+n = 4; %definisco il numero di variabili decisionali
+A = [1 1 1 1]; %definisco la matrice A dei vincoli di disuguaglianza
+b = [50]; %definisco il vettore B dei termini noti dei vincoli di disuguaglianza
+Aeq = [4 0 3 0]; %definisco la matrice dei vincoli di uguaglianza
+beq = [60]; %definisco il vettore dei termini noti dei vincoli di uguaglianza
+lb = 1*ones(n,1); %definisco il lower bound
+ub = 10*ones(n,1); %definisco l'upper bound
+x_0 = [3 3 3 3]'; %definisco la condizione iniziale
+options = optimoptions('fmincon','Algorithm','interior-point','Display','iter-detailed','PlotFcn','optimplotfval');
+[x, fval, exitflag, output] = fmincon (@(x)funzione_obiettivo(x), x_0, A, b, Aeq, beq, lb, ub, @(x)v_nlineare(x), options);
+%stampo le soluzioni
+disp('The optimail solution is: ');
+disp(x);
+disp(['The maximum cost of production is $' num2str(fval)]);
+Funzione non lineare
+function [c ceq] = v_nlineare(x)
+ceq = [];
+c = x(1)^2 + x(2)^2 + x(3)^2 -20;
 end
-
-my_rho=acf(n_D+1:2*n_D);
-theta_AR=R\my_rho;
-%Identifico la componente AR
-
-for kk=1:n_D
-    PHI_AR(:,kk)=y(n_D+1-kk:N-kk);
+Funzione obiettivo
+function L = funzione_obiettivo(x)
+L = 5*x(1)^2*x(2) + 3*x(3)^3;
 end
-
-y_hat_AR=PHI_AR*theta_AR;
-
-%La stima della componente MA è:
-my_eta=y(n_D+1:N)-y_hat_AR;
-close all
-
-figure (4)
-autocorr(my_eta)
-
-figure(5)
-parcorr(my_eta)
-
-%Pongo il problema di identificare la componente MA 
-%parto con un ordine di tentativo
-n_eq=17;
-
-for kk=1:n_eq
-    PHI_eq(:,kk)=my_eta(n_eq+1-kk:end-kk);
-end
-
-theta_eq=PHI_eq\my_eta(n_eq+1:end);
-
-%Previsione dell'uscita di un AR
-eta_hat=PHI_eq*theta_eq;
-w_hat=my_eta(n_eq+1:end)-eta_hat;
-
-%Vedo se w_hat ha le caratteristiche statistiche di un rumore bianco;
-%calcolo l'autocorrelazione totale e faccio un test di bianchezza
-
-figure(6)
-autocorr(w_hat)
-[h_eta,p_eta]=lbqtest(w_hat)
-
-%Scarto le eta per le quali non ho le w precedenti
-my_eta=my_eta(n_eq+1:end);
-%Faccio la stima ai minimi quadrati
-n_N=3; %ordine della componente MA
-
-for kk=1:n_N
-    PHI_MA(:,kk)=w_hat(n_N+1-kk:end-kk);
-end
-
-theta_MA=PHI_MA\(my_eta(n_N+1:end)-w_hat(n_N+1:end));

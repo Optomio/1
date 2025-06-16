5bba0766fdada080eda48476710d72fe2407bdad
Optomio <alessandro.menini99@gmail.com>
2025-06-16 17:29:43 +0200

    mark.m

--- mark.m
+++ mark.m
@@ -1,15 +1,19 @@
 n = 3;
-rendimenti_attesi = [0.3 0.5 0.4];
-covarianza = [0.10 0.02 -0.04;
-              0.02 0.12 0;
-             -0.04 0 0.11];
-A = -rendimenti_attesi;   % vincolo: rendimento ≥ 0.1
-b = -0.1; %inseriamo il rendimento atteso
-Aeq = ones(1,n);
-beq = 1; %inseriamo il valore del capitale totale
-LB = zeros(n,1);
-X0 = ones(n,1)/n;  % oppure [0.2; 0.3; 0.5];
-options = optimoptions('fmincon', 'Algorithm', 'interior-point', 'Display', 'iter-detailed','PlotFcn', 'optimplotx', 'EnableFeasibilityMode', true);  % ATTIVATO
-[X, fval, exitflag] = fmincon(@(x) x'*covarianza*x, X0, A, b, Aeq, beq, LB, [], [], options);
-disp('Soluzione ottima:');
-disp(X)
+rendimenti_attesi = [0.3, 0.5, 0.4];
+covarianza = [0.10, 0.02, -0.04; 0.02, 0.12, 0; -0.04, 0, 0.11];
+
+Aeq = [rendimenti_attesi; ones(1,n)]; 
+beq = [0.1; 1]; %il primo termine è il rendimento atteso, il secondo è il toatale del capitale
+LB = -inf(n,1); % Short selling permesso
+X0 = ones(n,1)/n; % Punto iniziale uniforme
+
+options = optimoptions('fmincon', 'Algorithm', 'interior-point', ...
+                      'Display', 'iter-detailed', 'PlotFcn', 'optimplotx');
+
+[X, fval, exitflag] = fmincon(@(x) x'*covarianza*x, X0, [], [], Aeq, beq, LB, [], [], options);
+
+% Verifica dei risultati
+disp('Pesi ottimali:'); disp(X);
+disp(['Varianza minima: ', num2str(fval)]);
+disp(['Rendimento atteso: ', num2str(rendimenti_attesi * X)]);
+disp(['Somma pesi: ', num2str(sum(X))]);
